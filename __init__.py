b7274e7acacdddc0ae5ad0e57ff3dd91977339d2
Emily Kirke <emilykirke@Emily-Kirkes-MacBook-Pro.local>
2014-02-16 15:53:14 -0500

    Realized I hadn't checked in my latest changes. I'm not sure what state things are in, but figure I get them uploaded before I lost them.

--- __init__.py
+++ __init__.py
@@ -1,16 +1,29 @@
+import random
+import string
+import json
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 
+from simplekv.memory import DictStore
+from flaskext.kvsession import KVSessionExtension
+
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = { 'DB' : "my_tubble_log" }
-app.config["SECRET_KEY"] = "MyLittleS3cr4t"
+app.config["SECRET_KEY"] = "my_secret"
+app.debug = True
+app.secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+
+store = DictStore();
+KVSessionExtension(store, app)
+
+clientId = json.loads( open('oauth_secrets.json', 'r').read())['web']['client_id']
 
 db = MongoEngine(app)
 
 def register_blueprints(app):
     # Prevents circular imports
-    from LeagueMgr.views import posts
-    app.register_blueprint(posts)
+    from LeagueMgr.views import pages
+    app.register_blueprint(pages)
 
 register_blueprints(app)
 
--- views.py
+++ views.py
@@ -1,52 +1,118 @@
-from flask import Blueprint, request, redirect, render_template, url_for
+import random
+import string
+import json
+from flask import Blueprint, request, redirect, render_template, url_for, session, make_response
 from flask.views import MethodView
-from LeagueMgr.models import Post, Comment
+from LeagueMgr.models import League, Participant
 from flask.ext.mongoengine.wtf import model_form
+from flask_peewee.utils import slugify
+from LeagueMgr import clientId
 
-posts = Blueprint('posts', __name__, template_folder='templates')
+pages = Blueprint('pages', __name__, template_folder='templates')
 
-class ListView(MethodView):
+class ProfileInfo(MethodView):
+    
+    def get(self):
+        userData = session.get('userData')
+        
+        data = {} 
+        if userData is None:
+            data['state'] = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+            session['state'] = data['state']
+            response = make_response( json.dumps( data ), 200 ) 
+            response.headers['Content-Type'] = 'application/json'
+            return response  
+        else:
+            data['userId'] = userData.userId
+            data['picture'] = userData.picture
+            data['displayName'] = userData.displayName
+
+            response = make_response( json.dumps( data ), 200 ) 
+            response.headers['Content-Type'] = 'application/json'
+            return response  
+
+    def post(self):        
+        if request.args.get('state', '') != session['state']:
+            response = make_response(json.dumps('Invalid state parameter.'), 401)
+            response.headers['Content-Type'] = 'application/json'
+            return response
+
+        data = request.get_json()
+        session['userData'] = data 
+
+        make_response( json.dumps('Stored user information' ) )
+        response.headers['Content-Type'] = 'application/json'
+        return response
+ 
+class LeagueListView(MethodView):
 
     def get(self):
-        posts = Post.objects.all()
-        return render_template('posts/list.html', posts=posts)
+        leagues = League.objects.limit(10)
+        return render_template('leagues/list.html', leagues=leagues)
 
+class LeagueCreateView(MethodView):
+    form = model_form( League, exclude=['created_at','slug','participants'] )
 
-class DetailView(MethodView):
-    form = model_form(Comment, exclude=['created_at'])
-    
-    def get_context(self, slug):
-        post = Post.objects.get_or_404(slug=slug)
+    def get(self):
+        league_form = self.form(request.form)
+        return render_template( 'leagues/create_league.html', form=league_form )
+
+    def post(self):
+        print( "Got the request" )
         form = self.form(request.form)
-        
-        context = {
-            "post": post,
-            "form": form
-        }
-        
-        return context
-
-    def get(self, slug):
-        context = self.get_context(slug)
-        return render_template('posts/detail.html', **context)
-
-    def post(self, slug):
-        context = self.get_context(slug)
-        form = context.get('form')
 
         if form.validate():
-            comment = Comment()
-            form.populate_obj(comment)
+            league = League()
+            form.populate_obj(league)
             
-            post = context.get('post')
-            post.comments.append(comment)
-            post.save()
+            league.slug = slugify( league.name )
+            league.save()
 
-            return redirect(url_for('posts.detail', slug=slug))
+            return redirect( url_for('pages.league_list') )
+        
+        return render_template( 'leagues/create_league.html', form=self.form )
+             
+class LeagueHomePage(MethodView):
 
-        return render_template('posts/detail.html', **context)
+    def get(self, slug):
+        league = League.objects.get_or_404(slug=slug)       
+        return render_template( 'leagues/league_home.html', league=league )
 
+class LeagueManagePage(MethodView):
+    form = model_form( Participant, exclude=['created_at','slug'] )
 
+    def get(self, slug):
+        league = League.objects.get_or_404(slug=slug)       
+
+        participant_form = self.form(request.form) 
+        return render_template( 'leagues/league_manage.html', league=league, participant_form=participant_form, slug=slug )
+
+class LeagueCreateParticipantPage(MethodView):
+    form = model_form( Participant, exclude=['created_at','slug'] )
+
+    def post(self, slug):
+        print( "Got post for ", slug )
+        form = self.form(request.form)
+
+        if form.validate():
+            participant = Participant()
+            form.populate_obj(participant)
+            
+            participant.slug = slugify( participant.name )
+
+            league = League.objects.get_or_404(slug=slug)
+            league.participants.append( participant )
+            league.save()
+
+            return ""
+        
+        return "" 
+ 
 # Register the urls
-posts.add_url_rule('/', view_func=ListView.as_view('list'))
-posts.add_url_rule('/<slug>/', view_func=DetailView.as_view('detail'))
+pages.add_url_rule('/', view_func=LeagueListView.as_view('league_list'))
+pages.add_url_rule('/create_league', view_func=LeagueCreateView.as_view('league_create'))
+pages.add_url_rule('/league/<slug>/', view_func=LeagueHomePage.as_view('league_home'))
+pages.add_url_rule('/league_manage/<slug>/', view_func=LeagueManagePage.as_view('league_manage'))
+pages.add_url_rule('/league_manage/<slug>/create_participant', view_func=LeagueCreateParticipantPage.as_view('create_participant'))
+
+pages.add_url_rule('/profileInfo', view_func=ProfileInfo.as_view('profile_info'))
